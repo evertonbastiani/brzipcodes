368f70ddad04008c15d8425287298910afb06e71
Everton Bastiani <evertonbastiani@gmail.com>
2023-09-02 13:33:43 -0300

    Adding pagination to table

---
+++ Presentation/web.ceps/wwwroot/scripts/ZipcodesDataTable.js
@@ -0,0 +1,11 @@
+﻿function CreateDataTable(tableId) {
+    $(document).ready(function(){
+        $(tableId).DataTable();
+    });
+}
+
+function DestroyDataTable(tableId) {
+    $(document).ready(function () {
+        $(tableId).DataTable().destroy();
+    });
+}
